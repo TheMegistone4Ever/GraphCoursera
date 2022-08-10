1cc20ba9fbcfac19f375c9ebab85b1f0122be4e3
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-08-10 12:05:00 +0300

    Добавьте файлы проекта.

---
+++ GraphCoursera/GraphCoursera.cpp
@@ -0,0 +1,16 @@
+﻿// ,
+// by Nickita Kyselyov, NTUU KPI nb. Igor Sikorsky, 1st grade, FICT, IS-13
+// ##.08.2022
+
+#include <iostream>
+
+// I don't use the entire namespace, but only this of the heap that is in it, because I don't need more
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+
+
+    return 0;
+}
