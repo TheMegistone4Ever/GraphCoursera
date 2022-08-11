044bb695802024619c74d3e5ef2c6a55a02fdcc8
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-08-11 13:42:55 +0300

    ALMOST DONE + DOC

--- GraphCoursera/Graph.h
+++ GraphCoursera/Graph.h
@@ -1,21 +1,21 @@
 #pragma once
-#include<iostream>
+#include <iostream>
 #include <list>
 #include <iterator>
+#include <iomanip>
+const double INF = 1000000;
 using namespace std;
-
 inline double drand(double d, double u) { return d + (static_cast<double>(rand()) / RAND_MAX) * (u - d); }
-
 class Graph {
     int V;
-    list<pair<int, int> >* adjacencyList;
+    list<pair<int, double> >* adjacencyList;
 public:
-    Graph(int V, double density, double dnLim, double upLim) : V(V), adjacencyList(new list<pair<int, int> >[V]) {
-        
-        // DO V * DENSITY in limits 
-
+    Graph(int V, double density, double dnLim, double upLim) : V(V), adjacencyList(new list<pair<int, double> >[V]) {
+        for (int i = 0; i < V; i++)
+            for (int e = 0; e < V * density; e++)
+                addEdge(i, rand() % V, drand(dnLim, upLim));
     }
-
+    ~Graph() { delete[] adjacencyList; }
 
     // add an edge in an undirected graph
     void addEdge(int u, int v, double weight) {
@@ -23,18 +23,45 @@ public:
         adjacencyList[v].push_back(make_pair(u, weight));
     }
 
-
     // print the adjacency list representation of graph
     void printAdjacencyList() {
         for (int v = 0; v < V; v++) {
             cout << "Vertex " << v << ':';
-            for (pair<int, int> p : adjacencyList[v]) cout << " -> " << p.first << ":w=" << p.second;
+            for (const pair<int, double>& p : adjacencyList[v])
+                cout << " -> " << setw(2) << p.first << ":w=" << setprecision(3) << p.second;
             cout << endl;
         }
     }
 
+    int minDistance(double* dist, bool* visited) {
+        double min = INF, index = 0;
+        for (int i = 0; i < V; i++)
+            if (dist[i] < min && !visited[i]) { min = dist[i]; index = i; }
+        return index;
+    }
 
-    int* dijkstra(int source) {
-        return NULL;
+    double** dijkstra(int source) {
+        double* dist = new double[V], * prev = new double[V];
+        bool* visited = new bool[V];
+        for (int i = 0; i < V; i++) {
+            dist[i] = INF;
+            prev[i] = -1;
+            visited[i] = false;
+        }
+        dist[source] = 0;
+        prev[source] = source;
+        for (int i = 0; i < V - 1; i++) {
+            int u = minDistance(dist, visited);
+            visited[u] = true;
+            for (list<pair<int, double> >::iterator it = adjacencyList[u].begin(); it != adjacencyList[u].end(); it++)
+                if (!visited[it->first] && dist[u] != INF && dist[it->first] > dist[u] + it->second) {
+                    dist[it->first] = dist[u] + it->second;
+                    prev[it->first] = u;
+                }
+        }
+        double** res = new double* [2];
+        res[0] = dist;
+        res[1] = prev;
+        return res;
     }
 };
--- GraphCoursera/GraphCoursera.cpp
+++ GraphCoursera/GraphCoursera.cpp
@@ -1,25 +1,29 @@
 ﻿// ,
-// by Nickita Kyselyov, NTUU KPI nb. Igor Sikorsky, 1st grade, FICT, IS-13
+// by Nickita Kyselyov, NTUU KPI nb. Igor Sikorsky, 1st grade, FICT, IS-13,
+// 11.08.2022
 #include "Graph.h"
-
 int main(int argc, char const* argv[]) {
     srand(time(NULL));
-    int V; cout << "input V: "; cin >> V;
-    double density; cout << "input density: "; cin >> density;
-    double dnLim; cout << "input dnLim: "; cin >> dnLim;
-    double upLim; cout << "input upLim: "; cin >> upLim;
+    int V; cout << "Input V: "; cin >> V;
+    double density; cout << "Input density: "; cin >> density;
+    double dnLim; cout << "Input dnLim: "; cin >> dnLim;
+    double upLim; cout << "Input upLim: "; cin >> upLim;
+    int source; cout << "Input source vertice: "; cin >> source;
 
     Graph graph(V, density, dnLim, upLim);
-    graph.addEdge(0, 1, 2);
+    /*graph.addEdge(0, 1, 2);
     graph.addEdge(0, 2, 4);
     graph.addEdge(1, 2, 3);
     graph.addEdge(1, 3, 4);
     graph.addEdge(1, 4, 5);
     graph.addEdge(2, 3, 2);
     graph.addEdge(2, 4, 3);
-    graph.addEdge(3, 4, 1);
+    graph.addEdge(3, 4, 1);*/
     graph.printAdjacencyList();
-    graph.dijkstra(0);
-
+    double** dijkstraInfo = graph.dijkstra(source);
+    for (int i = 0; i < V; i++)
+        cout << source << "\t->\t"
+        << i << "\tMin dist:\t" << setprecision(5) << dijkstraInfo[0][i]
+        << "\tV Prev:\t" << dijkstraInfo[1][i] << endl;
     return 0;
 }
